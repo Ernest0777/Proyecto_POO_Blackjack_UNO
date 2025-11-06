f82241b976749672b19ffbcd71c7db539f1d00e9
Pedrukis43 <15245544@modelo.edu.mx>
2025-11-05 19:53:32 -0600

    Interfaz Juego
    Interfaz de la parte de juego

--- BlackJack_UNO/Interfaces/IJuego.cs
+++ BlackJack_UNO/Interfaces/IJuego.cs
@@ -4,5 +4,15 @@ namespace BlackJack_1.Interfaces;
 
 public interface IJuego
 {
+    void IniciarJuego();
+    void RepartirCartas();
+    void JugarTurno();
+    void MostrarEstado();
+    void DeterminarGanador();
 
+    string NombreJuego { get; set; }
+    List<IJugador> Jugadores { get; set; }
+    List<ICarta> Mazo { get; set; }
+    List<ICarta> Descarte { get; set; }
+    int TurnoActual { get; set; }
 }
