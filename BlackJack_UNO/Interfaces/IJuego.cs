bc5f078117d7bb0cbc9ea69c3911cc7340bcb215
Ernest0777 <contacto2463@gmail.com>
2025-11-05 20:09:18 -0600

    Revisión de la interfaz de juego
     Se ajustó la interfaz para heredar de IEstadoJuego y mantener las responsabilidades separadas
    y en general se volvió mas limpio ya que no tiene funcionalidades que no le corresponden.

--- BlackJack_UNO/Interfaces/IJuego.cs
+++ BlackJack_UNO/Interfaces/IJuego.cs
@@ -2,17 +2,12 @@ using System;
 
 namespace BlackJack_1.Interfaces;
 
-public interface IJuego
+public interface IJuego: IEstadoJuego
 {
     void IniciarJuego();
     void RepartirCartas();
     void JugarTurno();
     void MostrarEstado();
     void DeterminarGanador();
-
-    string NombreJuego { get; set; }
-    List<IJugador> Jugadores { get; set; }
-    List<ICarta> Mazo { get; set; }
-    List<ICarta> Descarte { get; set; }
-    int TurnoActual { get; set; }
+    void FinalizarJuego();
 }
