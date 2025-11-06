8a78154bec2f63280debce18be687f37f25622f3
Ernest0777 <contacto2463@gmail.com>
2025-11-05 19:48:57 -0600

    Creación de la interfaz de jugador
    Se siguió la estructura planeada hasta cierto punto, añadí algunas cosas como
    IEstrategiaJugador: especificare mas de este en su debido commit pero es para permitir diferentes tipos de comportamiento de jugador
    Método ReportarAccion: Para el registro de acciones
    Método Getid: para identificar a cada jugador
    E hice un ajuste a Getmano para mejorar la seguridad del acceso a las cartas

--- BlackJack_UNO/Interfaces/IJugador.cs
+++ BlackJack_UNO/Interfaces/IJugador.cs
@@ -1,8 +1,20 @@
-using System;
+using System.Collections.Generic;
 
-namespace BlackJack_1.Interfaces;
-
+namespace BlackJack_1.Interfaces
+{
 public interface IJugador
 {
+        void RecibirCarta(ICarta carta);
+        void MostrarMano();
+        void TomarDecision(IJuego juegoContexto);
+        int ObtenerPuntos();
+        string GetNombre();
+        IReadOnlyList<ICarta> GetMano();
 
+        IEstrategiaJugador Estrategia { get; set; }
+
+        void ReportarAccion(string mensaje);
+
+        int GetId();
+    }
 }
