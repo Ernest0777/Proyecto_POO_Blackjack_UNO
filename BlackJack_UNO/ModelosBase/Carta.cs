a4801c87acd86c5720889ad80e1b5df3fbc45196
Ernest0777 <contacto2463@gmail.com>
2025-11-06 07:52:25 -0600

    Update Carta.cs
    Se declaró como clase abstracta para evitar instanciación directa
    Se implementaron propiedades Color, Valor y Tipo con protected init para permitir inicialización controlada desde subclases Se agregó constructor para asegurar la creación de cartas válidas con sus valores definidos.
    Y más cosas que descubrirán cuando abran esta madre porque ya me canse de escribir.

--- BlackJack_UNO/ModelosBase/Carta.cs
+++ BlackJack_UNO/ModelosBase/Carta.cs
@@ -1,8 +1,27 @@
 using System;
-
+using BlackJack_1.Interfaces;
+using BlackJack_1.ModelosBase;
 namespace BlackJack_1.ModelosBase;
 
-public class Carta
-{
+    public class Carta: ICarta
+    {
+ public string Color { get;protected init; }
+        public string Valor { get; protected init; }
+        public string Tipo { get; protected init; }
 
+        protected Carta(string color, string valor, string tipo)
+        {
+            Color = color;
+            Valor = valor;
+            Tipo = tipo;
+        }
+
+    public virtual int GetValorNumerico() => 0;
+    
+
+         public virtual void MostrarCarta()
+        => Console.WriteLine(ToString());
+
+        public override string ToString()
+        => $"{Valor} de {Color} ({Tipo})";
 }
