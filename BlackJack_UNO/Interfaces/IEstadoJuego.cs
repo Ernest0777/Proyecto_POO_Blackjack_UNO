5a65459ab1e781326281624282c37030ae261a7c
Ernest0777 <contacto2463@gmail.com>
2025-11-05 20:08:05 -0600

    Creación de la interfaz IEstadoJuego
    Contiene información general como el nombre del juego, los jugadores, el mazo, el descarte y el turno actual.
    Permite acceder al estado del juego sin exponer la lógica interna.

---
+++ BlackJack_UNO/Interfaces/IEstadoJuego.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace BlackJack_1.Interfaces;
+
+public interface IEstadoJuego
+{
+    string NombreJuego { get; }
+    IReadOnlyList<IJugador> Jugadores { get; }
+    IReadOnlyList<ICarta> Mazo { get; }
+    IReadOnlyList<ICarta> Descarte { get; }
+    int TurnoActual { get; }
+
+}
