77d8a4410b3350b15101021324bbb661fe2b820c
Ernest0777 <contacto2463@gmail.com>
2025-11-06 10:25:35 -0600

    Update IJuego.cs
    Cambie algunas cosas, pero la mas importante fue que ya no hereda de estado de juego porque rompe los principios SOLID y en general para las cosas que haremos mas adelante será mas sencillo de esta manera.

--- BlackJack_UNO/Interfaces/IJuego.cs
+++ BlackJack_UNO/Interfaces/IJuego.cs
@@ -2,12 +2,21 @@ using System;
 
 namespace BlackJack_1.Interfaces;
 
-public interface IJuego: IEstadoJuego
+public interface IJuego
 {
     void IniciarJuego();
     void RepartirCartas();
     void JugarTurno();
-    void MostrarEstado();
+    void AvanzarTurno();
     void DeterminarGanador();
     void FinalizarJuego();
+
+    IEstadoJuego EstadoActual{ get; }
+
+    string ObtenerEstado();
+    void RegistrarAccion(string descripcion);
+
+        //Permite notificar a un Logger u observador sin acoplarlo directamente
+        event Action<string>? OnAccionRegistrada;
+
 }
