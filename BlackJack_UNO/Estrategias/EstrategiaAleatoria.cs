7ab53123cdc5c59086e57306fd24a3af08373920
Ernest0777 <contacto2463@gmail.com>
2025-11-10 22:08:10 -0600

    Update EstrategiaAleatoria.cs
    Modelamos la primera estrategia para uno, siendo la aleatoria que solo coloca una carta valida al azar

--- BlackJack_UNO/Estrategias/EstrategiaAleatoria.cs
+++ BlackJack_UNO/Estrategias/EstrategiaAleatoria.cs
@@ -1,8 +1,29 @@
-using System;
-
 namespace BlackJack_1.Estrategias;
 
-public class EstrategiaAleatoria
-{
+using BlackJack_1.Interfaces;
+using BlackJack_1.Juegos.Uno;
+using BlackJack_1.Utilidades;
 
+// Estrategia que elige una carta tuene que ser valida al azar
+public class EstrategiaAleatoria : IEstrategiaJugador
+{
+    private readonly Random generadorAleatorio = new();
+
+    //elige una aleatoria que pueda colocar
+    public CartaUno? DecidirCarta(JugadorUno jugador, CartaUno cartaSuperior)
+    {
+        // Obtener las cartas que puede jugar
+        var cartasJugables = jugador.ObtenerMano()
+            .OfType<CartaUno>()
+            .Where(carta => ReglasUno.PuedeJugar(cartaSuperior, carta))
+            .ToList();
+
+        // Si no tiene ninguna carta valida devuelve null y toma carta
+        if (cartasJugables.Count == 0)
+            return null;
+
+        // Elegir una carta al azar entre las disponibles
+        int indiceAleatorio = generadorAleatorio.Next(cartasJugables.Count);
+        return cartasJugables[indiceAleatorio];
+    }
 }
