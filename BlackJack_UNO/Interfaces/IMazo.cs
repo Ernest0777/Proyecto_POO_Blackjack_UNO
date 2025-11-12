596d1fb5ba592e95276120fcc3d545d1979974c3
Ernest0777 <contacto2463@gmail.com>
2025-11-11 19:25:58 -0600

    Update IMazo.cs

--- BlackJack_UNO/Interfaces/IMazo.cs
+++ BlackJack_UNO/Interfaces/IMazo.cs
@@ -1,12 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 namespace BlackJack_1.Interfaces;
 
 public interface IMazo
 {
-void Barajar();
+    // Mezcla las cartas actuales del mazo
+    void Barajar();
 
+    // Devuelve la cantidad de cartas restantes
     int CartasRestantes();
 
+    // Saca la carta superior del mazo
+    ICarta SacarCarta();
+
+    // Devuelve una lista con las cartas actuales solo para pruebas 
     IEnumerable<ICarta> MostrarCartas();
 }
