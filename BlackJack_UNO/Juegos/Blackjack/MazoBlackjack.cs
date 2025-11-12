7db75d2c92e3b541563af5a86c4b647731e5be5d
Ernest0777 <contacto2463@gmail.com>
2025-11-11 19:46:06 -0600

    Update MazoBlackjack.cs
    No habia interfaz de mazo por lo que pues no tenia sentido crear esta clase antes pero bueno, ya quedo corregida

--- BlackJack_UNO/Juegos/Blackjack/MazoBlackjack.cs
+++ BlackJack_UNO/Juegos/Blackjack/MazoBlackjack.cs
@@ -1,58 +1,24 @@
+namespace BlackJack_1.Juegos.Blackjack;
+
 using System.Collections.Generic;
-using System.Linq;
-using BlackJack_1.Utilidades;
+using BlackJack_1.ModelosBase;
 using BlackJack_1.Interfaces;
 
-namespace BlackJack_1.Juegos.Blackjack
-{
-    public class MazoBlackjack
+public class MazoBlackjack : Mazo<CartaBlackjack>, IMazo
     {
-        private readonly List<CartaBlackjack> _cartas;
-        private int _indice;
+       public MazoBlackjack()
+    {
+        GenerarMazo();
+        Barajar(); 
+    }
 
-        public MazoBlackjack()
-        {
-            _cartas = GenerarMazo();
-            Mezclar();
-        }
+          private void GenerarMazo()
+    {
+        string[] figuras = { "Corazones", "Picas", "Treboles", "Diamantes" };
+        string[] valores = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 
-        private List<CartaBlackjack> GenerarMazo()
-        {
-            string[] figuras = { "Corazones", "Picas", "Tréboles", "Diamantes" };
-            string[] valores = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
-
-            var cartas = new List<CartaBlackjack>();
-            foreach (var figura in figuras)
-                foreach (var valor in valores)
-                    cartas.Add(new CartaBlackjack(figura, valor, "Blackjack"));
-
-            return cartas;
-        }
-
-        public void Mezclar()
-        {
-            var mezcladas = Randomizador.BarajarLista(_cartas);
-            _cartas.Clear();
-            _cartas.AddRange(mezcladas);
-            _indice = 0;
-        }
-
-        public CartaBlackjack RobarCarta()
-        {
-            if (_indice >= _cartas.Count)
-                Mezclar();
-
-            return _cartas[_indice++];
-        }
-
-        public int CartasRestantes => _cartas.Count - _indice;
-
-        public IReadOnlyList<ICarta> ObtenerCartasRestantes()
-        {
-            return _cartas.Skip(_indice).Cast<ICarta>().ToList().AsReadOnly();
-        }
+        foreach (var figura in figuras)
+            foreach (var valor in valores)
+                AgregarCarta(new CartaBlackjack(figura, valor, "Blackjack"));
     }
 }
-
-
-
