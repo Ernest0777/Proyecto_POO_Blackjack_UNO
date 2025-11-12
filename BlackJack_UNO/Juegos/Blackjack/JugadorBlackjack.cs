efb55e8a8c60b298f75ad3403ce12d9c3d1b3287
Ernest0777 <contacto2463@gmail.com>
2025-11-11 19:52:43 -0600

    Update JugadorBlackjack.cs

--- BlackJack_UNO/Juegos/Blackjack/JugadorBlackjack.cs
+++ BlackJack_UNO/Juegos/Blackjack/JugadorBlackjack.cs
@@ -2,27 +2,43 @@ using System.Linq;
 using BlackJack_1.ModelosBase;
 using BlackJack_1.Interfaces;
 
-namespace BlackJack_1.Juegos.Blackjack
-{
-    public class JugadorBlackjack : JugadorBase
+namespace BlackJack_1.Juegos.Blackjack;
+
+public class JugadorBlackjack : JugadorBase, IJugadorBlackjack
+   {
+    public IEstrategiaJugadorBlackjack Estrategia { get; set; }
+
+    // constructor recibe id nombre y la estrategia concreta
+    public JugadorBlackjack(int id, string nombre, IEstrategiaJugadorBlackjack estrategia)
+        : base(id, nombre)
     {
-        public JugadorBlackjack(int id, string nombre, IEstrategiaJugador estrategia)
-            : base(id, nombre, estrategia) { }
+        Estrategia = estrategia;
+    }
 
-        public override int ObtenerPuntos()
+    // dependiendo de la estrategia toma una decision
+    public override void TomarDecision(IJuego juegoContexto)
+    {
+        if (Estrategia != null)
+            Estrategia.EjecutarDecision(this, juegoContexto);
+        else
+            NotificarAccion("no tiene estrategia definida y pasa su turno.");
+    }
+
+    // Calcula los puntos de la mano considerando los as
+    public override int ObtenerPuntos()
+    {
+        int puntosTotales = _mano.Sum(c => c.ObtenerValorNumerico());
+        int cantidadAses = _mano.Count(c => c is CartaBlackjack carta && carta.Valor == "A");
+
+        while (puntosTotales > 21 && cantidadAses > 0)
         {
-            int puntosTotales = _mano.Sum(c => c.ObtenerValorNumerico());
-            int ases = _mano.Count(c => c is CartaBlackjack cb && cb.Valor == "A");
-
-            while (puntosTotales > 21 && ases > 0)
-            {
-                puntosTotales -= 10; // El As pasa de valer 11 a valer 1
-                ases--;
-            }
-
-            return puntosTotales;
+            puntosTotales -= 10; // El As pasa de valer 11 a valer 1
+            cantidadAses--;
         }
+
+        return puntosTotales;
     }
 }
+
 
 
